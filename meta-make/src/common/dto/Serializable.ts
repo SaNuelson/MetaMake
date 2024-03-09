b3bef6aa89bfe5a07f8388674689fa9446a560aa
Samuel Novelinka <sanuelson42@gmail.com>
2024-03-09 19:08:43 +0100

    potentially working version of deserializer

--- meta-make/src/common/dto/Serializable.ts
+++ meta-make/src/common/dto/Serializable.ts
@@ -1,9 +1,9 @@
-import { Target } from "electron-builder";
+export default class Serializable {
 
-export default class Serializable<Source extends Object> {
+  constructor(..._: any[]) { }
 
-  protected static __classDict: {[className: string]: typeof Serializable<any>} = {};
-  public static addClass(clazz: typeof Serializable<any>, sourceName: string | null = null) {
+  protected static __classDict: {[className: string]: typeof Serializable} = {};
+  public static addClass(clazz: typeof Serializable, sourceName: string | null = null) {
     const name = sourceName ?? clazz.name;
     if (Serializable.__classDict.hasOwnProperty(name))
       return;
@@ -13,14 +13,7 @@ export default class Serializable<Source extends Object> {
   // @ts-ignore
   private __className: string = this.constructor.name;
 
-  constructor(serialized: Source) {
-    Object.assign(this, serialized);
-    for (const propName of Object.getOwnPropertyNames(this)) {
-      this[propName] = Serializable.deserialize(this[propName]);
-    }
-  }
-
-  public static deserialize(serialized: any): Serializable<any> | Object | any /*primitive*/ {
+  public static deserialize(serialized: any): Serializable | Object | any /*primitive*/ {
     console.log("Serializable.deserialize", serialized);
 
     // primitive, keep as is
@@ -31,7 +24,12 @@ export default class Serializable<Source extends Object> {
 
     // serialized, deserialize via constructor
     if (serialized["__className"] !== undefined) {
-      return new Serializable.__classDict[serialized.__className](serialized);
+      let desereliazed = new Serializable.__classDict[serialized.__className]();
+      Object.assign(desereliazed, serialized);
+      for (const key of Object.keys(desereliazed)) {
+        desereliazed[key] = Serializable.deserialize(desereliazed[key]);
+      }
+      return desereliazed;
     }
 
     // JS built-in, may contain serialized items
@@ -43,42 +41,47 @@ export default class Serializable<Source extends Object> {
   }
 }
 
-class Integer extends Serializable<Integer> {
+class Integer extends Serializable {
   public value: number;
-
-  constructor(serialized: Integer) {
-    console.log(`Integer.constructor(${JSON.stringify(serialized)})`);
-    super(serialized);
-    this.value ??= -999;
+  constructor(value: number) {
+    super();
+    this.value = value;
+  }
+  public negative(): number {
+    return -this.value;
   }
 }
 Serializable.addClass(Integer);
 
-const integer = new Integer({value: 5} as Integer);
-console.log(integer);
-console.log(integer.value);
+const sourceInteger = new Integer(25);
+const serializedInteger = structuredClone(sourceInteger);
+const deserializedInteger = Serializable.deserialize(serializedInteger);
+console.log(deserializedInteger);
+console.log(deserializedInteger.negative());
 
-class ArrayList extends Serializable<ArrayList> {
-  public items: Serializable<any>[];
-  constructor(serialized: ArrayList) {
-    console.log(`ArrayList.constructor(${JSON.stringify(serialized)})`);
-    super(serialized);
-    this.items ??= [];
+class ArrayList extends Serializable {
+  public items: any[];
+  constructor(items: any[]) {
+    super();
+    this.items = items ?? [];
+  }
+  public reversed(): any[] {
+    let copy = [...this.items];
+    return copy.reverse();
   }
 }
 Serializable.addClass(ArrayList);
 
-const list = new ArrayList(
-  {
-    __className: "ArrayList",
-    items: [
-      {value: 10, __className: "Integer"},
-      {value: 20, __className: "Integer"}
-    ]
-  } as unknown as ArrayList);
-console.log(list);
-console.log(list.items);
-console.log(list.items[0])
+const list = new ArrayList([
+  new Integer(5),
+  new Integer(25),
+  125
+]);
+const serializedList = structuredClone(list);
+const deserializedList = Serializable.deserialize(serializedList);
+console.log(deserializedList);
+console.log(deserializedList.reversed())
+console.log(deserializedList.items[0].negative());
 
 interface Person {
   name: string;
@@ -87,11 +90,12 @@ interface Person {
   greet(): string;
 }
 
-class SourcePerson extends Serializable<SourcePerson> implements Person {
+class SourcePerson extends Serializable implements Person {
   age: number;
   name: string;
 
   constructor(name: string, age: number) {
+    super();
     this.name = name;
     this.age = age;
   }
@@ -101,7 +105,7 @@ class SourcePerson extends Serializable<SourcePerson> implements Person {
   }
 }
 
-class TargetPerson extends Serializable<SourcePerson> implements Person {
+class TargetPerson extends Serializable implements Person {
   age: number;
   name: string;
 
@@ -120,6 +124,13 @@ class TargetPerson extends Serializable<SourcePerson> implements Person {
   }
 }
 Serializable.addClass(TargetPerson, SourcePerson.name);
+
+const person: SourcePerson = new SourcePerson("John", 20);
+console.log(person.greet());
+const serializedPerson = structuredClone(person);
+console.log(serializedPerson.greet === undefined);
+const deserializedPerson = Serializable.deserialize(serializedPerson);
+console.log(deserializedPerson.greet());
 
 const sourcePerson: SourcePerson = new SourcePerson("John", 15);
 console.log(sourcePerson.greet());
