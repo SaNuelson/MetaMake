373240d2e66d4ffb7a8f6a44b69459efcfc3f179
Samuel Novelinka <sanuelson42@gmail.com>
2025-05-30 18:10:12 +0200

    Improved vocabulary, added vocab utils

--- src/memory/utils.ts
+++ src/memory/utils.ts
@@ -1,6 +1,6 @@
-import { DataFactory, Literal } from 'n3';
+import { DataFactory, Literal, NamedNode } from 'n3';
 import literal = DataFactory.literal;
-import { dateTimeType, dateType } from './vocabulary';
+import { dateTimeType, dateType, namespaceToPrefix } from './vocabulary';
 
 const dateFormatter = new Intl.DateTimeFormat('en', {year: 'numeric', month: '2-digit', day: '2-digit'});
 
@@ -12,4 +12,33 @@ export function dateLiteral(date: Date): Literal {
 export function dateTimeLiteral(dateTime: Date): Literal {
     const formattedDateTime = dateTime.toISOString();
     return literal(formattedDateTime, dateTimeType);
+}
+
+export function splitUri(node: NamedNode): string[] {
+    let uri = node.value;
+
+    if (uri.endsWith('/'))
+        uri = uri.substring(0, uri.length - 1);
+
+    const hashIndex = uri.lastIndexOf('#');
+    const slashIndex = uri.lastIndexOf('/');
+    const splitIndex = Math.max(hashIndex, slashIndex);
+
+    return [uri.substring(0, splitIndex), uri.substring(splitIndex + 1)]
+}
+
+export function getNamespace(node: NamedNode): string {
+    return splitUri(node)[0];
+}
+
+export function getLocalName(node: NamedNode): string {
+    return splitUri(node)[1];
+}
+
+export function getCompactName(node: NamedNode): string {
+    const [namespace, localName] = splitUri(node);
+    const prefix = namespaceToPrefix[namespace];
+    if (!prefix)
+        return node.value;
+    return prefix + ':' + localName;
 }
--- src/memory/vocabulary.ts
+++ src/memory/vocabulary.ts
@@ -1,14 +1,80 @@
-import { Util } from 'n3';
+import { NamedNode, PrefixedToIri, Util, Writer } from 'n3';
 
 import prefix = Util.prefix;
+import { logger } from '../logger';
+import prefixes = Util.prefixes;
 
-const dbo = prefix('https://dbpedia.org/ontology/');
+export const dbo = prefix('https://dbpedia.org/ontology/');
+export const rdfs = prefix('https://www.w3.org/TR/rdf-schema/#');
+export const dct = prefix('http://purl.org/dc/terms/');
+export const xsd = prefix('http://www.w3.org/2001/XMLSchema#');
+export const csvw = prefix('http://www.w3.org/ns/csvw#');
+export const mm = prefix('http://tempuri.org/metamake/');
+export const adms = prefix('http://www.w3.org/ns/adms#');
+export const dcat = prefix('http://www.w3.org/ns/dcat#');
+export const dcatAp = prefix('http://data.europa.eu/r5r/');
+export const dcterms = prefix('http://purl.org/dc/terms/');
+export const foaf = prefix('http://xmlns.com/foaf/0.1/');
+export const location = prefix('http://www.w3.org/ns/locn#');
+export const odrl = prefix('http://www.w3.org/ns/odrl/2/');
+export const prov = prefix('http://www.w3.org/ns/prov#');
+export const pu = prefix('https://data.gov.cz/slovník/podmínky-užití/');
+export const skos = prefix('http://www.w3.org/2004/02/skos/core#');
+export const spdx = prefix('http://spdx.org/rdf/terms#');
+export const time = prefix('http://www.w3.org/2006/time#');
+export const vcard = prefix('http://www.w3.org/2006/vcard/ns#');
+export const nkod = prefix('https://data.gov.cz/slovník/nkod/');
+
+export const prefixToNamespace = {
+    dbo,
+    rdfs,
+    dct,
+    xsd,
+    csvw,
+    mm,
+    adms,
+    dcat,
+    dcatAp,
+    dcterms,
+    foaf,
+    location,
+    odrl,
+    prov,
+    pu,
+    skos,
+    spdx,
+    time,
+    vcard,
+    nkod,
+}
+
+export const namespaceToPrefix = Object.fromEntries(
+    Object.entries(prefixToNamespace).map(([prefix, uri]) => [uri, prefix])
+);
+
+/**
+ * Add a prefix with its namespace to the lookup table. Used in logging and provenance.
+ * @param prefix short-hand alias for the namespace (i.e. 'dcat').
+ * @param uri full uri of the namespace (i.e. 'http://www.w3.org/ns/dcat#').
+ * @returns true if prefix was added, false if it was already present (or conflicting).
+ */
+export function addPrefix(prefix: string, uri: string): boolean {
+    if (prefix in prefixes) {
+        if (prefixes[prefix] !== uri)
+            logger.error(`Prefix ${prefix} already exists with different URI ${prefixes[prefix]} and ${uri}.`);
+        return false;
+    }
+
+    prefixes[prefix] = uri;
+    return true;
+}
+
+
 /** https://dbpedia.org/ontology/filename */
 export const fileName = dbo('filename');
 /** https://dbpedia.org/ontology/file */
 export const file = dbo('file');
 
-const rdfs = prefix('https://www.w3.org/TR/rdf-schema/#');
 /** https://www.w3.org/TR/rdf-schema/#type */
 export const type = rdfs('type');
 /** https://www.w3.org/TR/rdf-schema/#type */
@@ -16,7 +82,6 @@ export const isA = rdfs('type');
 /** https://www.w3.org/TR/rdf-schema/#resource */
 export const resource = rdfs('resource');
 
-const dct = prefix('http://purl.org/dc/terms/');
 /** http://purl.org/dc/terms/title */
 export const title = dct('title');
 /** http://purl.org/dc/terms/description */
@@ -26,13 +91,11 @@ export const created = dct('created');
 /** http://purl.org/dc/terms/modified */
 export const modified = dct('modified');
 
-const xsd = prefix('http://www.w3.org/2001/XMLSchema#');
 /** http://www.w3.org/2001/XMLSchema#dateTime */
 export const dateTimeType = xsd('dateTime');
 /** http://www.w3.org/2001/XMLSchema#date */
 export const dateType = xsd('date');
 
-const csvw = prefix('http://www.w3.org/ns/csvw#');
 /** http://www.w3.org/ns/csvw#schema */
 export const csvSchema = csvw('schema');
 /** http://www.w3.org/ns/csvw#TableSchema */
@@ -48,11 +111,6 @@ export const csvColumnUnique = csvw('unique');
 /** http://www.w3.org/ns/csvw#enum */
 export const csvColumnEnum = csvw('enum');
 
-export const mm = prefix('http://tempuri.org/metamake/');
-
-export const adms = prefix('http://www.w3.org/ns/adms#');
-
-export const dcat = prefix('http://www.w3.org/ns/dcat#');
 /** http://www.w3.org/ns/dcat#Catalog */
 export const catalog = dcat('Catalog');
 /** http://www.w3.org/ns/dcat#DatasetSeries */
@@ -100,13 +158,11 @@ export const endpointDescription = dcat('endpointDescription');
 /** http://www.w3.org/ns/dcat#servesDataset */
 export const servesDataset = dcat('servesDataset');
 
-export const dcatAp = prefix('http://data.europa.eu/r5r/');
 /** http://data.europa.eu/r5r/applicableLegislation */
 export const applicableLegislation = dcatAp('applicableLegislation');
 /** http://data.europa.eu/r5r/hvdCategory */
 export const hvdCategory = dcatAp('hvdCategory');
 
-export const dcterms = prefix('http://purl.org/dc/terms/');
 /** http://purl.org/dc/terms/publisher */
 export const publisher = dcterms('publisher');
 /** http://purl.org/dc/terms/accrualPeriodicity */
@@ -122,19 +178,11 @@ export const conformsTo = dcterms('conformsTo');
 /** http://purl.org/dc/terms/format */
 export const format = dcterms('format');
 
-export const foaf = prefix('http://xmlns.com/foaf/0.1/');
 /** http://xmlns.com/foaf/0.1/homepage */
 export const homepage = foaf('homepage');
 /** http://xmlns.com/foaf/0.1/page */
 export const page = foaf('page');
 
-export const location = prefix('http://www.w3.org/ns/locn#');
-
-export const odrl = prefix('http://www.w3.org/ns/odrl/2/');
-
-export const prov = prefix('http://www.w3.org/ns/prov#');
-
-export const pu = prefix('https://data.gov.cz/slovník/podmínky-užití/');
 /** https://data.gov.cz/slovník/podmínky-užití/specifikace */
 export const specifikace = pu('specifikace');
 /** https://data.gov.cz/slovník/podmínky-užití/autorské-dílo */
@@ -150,18 +198,9 @@ export const databaseChranenaZvlastnimiPracy = pu('databáze-chráněná-zvláš
 /** https://data.gov.cz/slovník/podmínky-užití/osobní-údaje */
 export const osobniUdaje = pu('osobní-údaje');
 
-export const skos = prefix('http://www.w3.org/2004/02/skos/core#');
-
-export const spdx = prefix('http://spdx.org/rdf/terms#');
-
-export const time = prefix('http://www.w3.org/2006/time#');
-
-export const vcard = prefix('http://www.w3.org/2006/vcard/ns#');
 /** http://www.w3.org/2006/vcard/ns#Organization */
 export const organization = vcard('Organization');
 /** http://www.w3.org/2006/vcard/ns#fn */
 export const fn = vcard('fn');
 /** http://www.w3.org/2006/vcard/ns#hasEmail */
 export const hasEmail = vcard('hasEmail');
-
-export const nkod = prefix('https://data.gov.cz/slovník/nkod/');
